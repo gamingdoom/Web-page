6d31b13ba5013697318ec70f9d2550eabbc4f35c
unknown <pranaysanghai@gmail.com>
2018-06-03 13:58:55 -0700

    initialll committ

---
+++ index.js
@@ -0,0 +1,33 @@
+ function userGame() 
+{
+    var link = 'game.html';
+    var number = 2;
+
+
+
+    var fn = document.getElementById("in").value;
+
+    var usergamesentence;
+    
+    if (fn == "teacher"){
+        
+       alert("ok, taking you to the appropiate page");
+        link = 'teacher.html';
+            window.location.assign(link);
+
+       
+    }
+    
+    else if(fn != "") {
+        alert("are you ready to play a game "+fn);
+            window.location.assign(link);
+
+        
+    
+    } else {
+        alert("please tell me your username");
+    }
+    
+ 
+
+}
